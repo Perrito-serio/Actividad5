991f78517622bf1d448324feab84c069c86ba2f4
WisommdCV <wilsondcv711@gmail.com>
2025-09-19 10:22:46 -0500

    CommitTrolleoENV

--- Teo-Em05/Program.cs
+++ Teo-Em05/Program.cs
@@ -1,8 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); //SWAG
+builder.Services.AddSwaggerGen(); //SWAG
 
 var app = builder.Build();
 
